4e0d78ad28a55fda65784bfdac90b1862d9aae3b
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2024-10-14 15:45:31 -0500

    add more todos for testing information

--- src/projectList.js
+++ src/projectList.js
@@ -2,8 +2,15 @@ import {Project} from "./Project.js";
 import {Todo} from "./Todo.js";
 
 const projectList = [(new Project("Default Project",[]))]; 
-const todo = new Todo("Add more Todos","More cards","2024-12-31","Medium");
-projectList[0].addTodo(todo);
+const todos = [
+    new Todo("Add no Todos","No cards","2024-12-31","Low"),
+    new Todo("Add a few Todos","Few cards","2024-12-31","Medium"),
+    new Todo("Add more Todos","More cards","2024-12-31","High"),
+    new Todo("Add lots Todos","Tons cards","2024-12-31","Critical"),
+];
+for(let i=0;i<todos.length;i++){
+    projectList[0].addTodo(todos[i]);
+}
 
 export function getProjectList(){
     return projectList;
